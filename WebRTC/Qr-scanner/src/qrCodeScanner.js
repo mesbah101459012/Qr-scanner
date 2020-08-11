002643ad1b77a661253cf5d1b561284eda98a0e5
Mahammad Mesbah <57636314+mesbah101459012@users.noreply.github.com>
2020-08-11 03:43:17 +0200

    sdfkog

--- WebRTC/Qr-scanner/src/qrCodeScanner.js
+++ WebRTC/Qr-scanner/src/qrCodeScanner.js
@@ -10,75 +10,72 @@ const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
 let res_qr = "";
-$(document).ready(function () {
+$(document).ready(function() {
 
-qrcode1.callback = res => {
-    if (res) {
-        outputData.innerText = res;
-		res_qr = res;
-        scanning = false;
-		
-        video.srcObject.getTracks().forEach(track => {
-            track.stop();
+    qrcode1.callback = res => {
+        if (res) {
+            outputData.innerText = res;
+            res_qr = res;
+            scanning = false;
+
+            video.srcObject.getTracks().forEach(track => {
+                track.stop();
+            });
+
+            qrResult.hidden = false;
+            canvasElement.hidden = true;
+            btnScanQR.hidden = false;
+            ajax_send();
+
+        }
+    };
+
+
+    var ajax_send = function() {
+
+        jQuery.ajax({
+            type: "POST",
+            url: "encryption_decryption.php",
+            data: {
+                response_qr: res_qr //$('outputData').val();
+            }
+        }).done(function(response) {
+            setTimeout(function() {
+                alert(response);
+            });
+
+
+        }); // end ajax
+    }
+
+    btnScanQR.onclick = () => {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+            scanning = true;
+            qrResult.hidden = true;
+            btnScanQR.hidden = true;
+            canvasElement.hidden = false;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.srcObject = stream;
+            video.play();
+            tick();
+            scan();
         });
+    };
 
-        qrResult.hidden = false;
-        canvasElement.hidden = true;
-        btnScanQR.hidden = false;
-		ajax_send();
-		
+    function tick() {
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+        scanning && requestAnimationFrame(tick);
     }
-};
 
-	
-	var ajax_send = function () {
-		
-		jQuery.ajax({
-			type: "POST",
-			url: "encryption_decryption.php",
-			data: {
-				response_qr: res_qr //$('outputData').val();
-			}
-		}).done(function (response) {
-			setTimeout(function () {
-				
-			});
-			alert(response);
-			if (response == "http://en.m.wikipedia.org") {
-				alert("ok");
-			} else {
-				alert("not okey");
-			}
-		}); // end ajax
-	}
-	
-btnScanQR.onclick = () => {
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-        scanning = true;
-        qrResult.hidden = true;
-        btnScanQR.hidden = true;
-        canvasElement.hidden = false;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        video.srcObject = stream;
-        video.play();
-        tick();
-        scan();
-    });
-};
-function tick() {
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
-    scanning && requestAnimationFrame(tick);
-}
-
-function scan() {
-    try {
-        qrcode1.decode();
-    } catch (e) {
-        setTimeout(scan, 300);
+    function scan() {
+        try {
+            qrcode1.decode();
+        } catch (e) {
+            setTimeout(scan, 300);
+        }
     }
-}
 
 });
